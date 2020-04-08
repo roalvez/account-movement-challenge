28ed587cf425b0fef659945b08b1b56c5a59bb4c
Ronaldo Alves <ronaldo@neomed.tech>
2020-04-07 22:56:17 -0300

    Add tactics to remove active logger when service is called

--- app/services/bank_balance_service.rb
+++ app/services/bank_balance_service.rb
@@ -1,14 +1,31 @@
 class BankBalanceService < ServiceBase
-  attr_reader :accounts_file, :transactions_file
+  attr_reader :accounts_file, :transactions_file, :old_logger
 
   def initialize(accounts_file, transactions_file)
     @accounts_file = accounts_file
     @transactions_file = transactions_file
+    @old_logger = ActiveRecord::Base.logger
   end
 
   def call
+    remove_logger
+
     CreateBankAccountService.call(accounts_file)
     UpdateBankAccountService.call(transactions_file)
     ShowBankAccountService.call
+
+    apply_logger
+  end
+
+  private
+
+  def remove_logger
+    ActiveRecord::Base.logger = nil
+  end
+
+  def apply_logger
+    ActiveRecord::Base.logger = old_logger
+
+    nil
   end
 end
