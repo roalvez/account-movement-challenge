54a1d94b3f1a66815506e362807d39d5d50ec321
Ronaldo Alves <ronaldo@neomed.tech>
2020-04-07 22:32:38 -0300

    Change penalty price callback on bank_account model

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,13 +2,13 @@ class BankAccount < ApplicationRecord
   validates :account_number, :balance, presence: true
   validates :account_number, uniqueness: true
 
-  before_update :negative_balance_tax
+  before_update :negative_balance_penalty
 
   private
 
   def negative_balance_penalty
     return if self.balance >= 0
 
-    self.balance -= 3
+    self.balance -= 300
   end
 end
